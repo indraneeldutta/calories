68d001c77c265d1ce2ffd1ce0e4471db048f1d14
Indraneel Dutta <indraneel.dutta@hotmail.com>
2020-06-06 23:14:59 +0530

    added env file and db config file

---
+++ db.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"context"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func init() {
+	// loads values from .env into the system
+	if err := godotenv.Load(); err != nil {
+		log.Print("No .env file found")
+	}
+}
+
+// GetClient connects to MongoDB
+func GetClient() *mongo.Client {
+	connect := os.Getenv("DB_CONN")
+	clientOptions := options.Client().ApplyURI(connect)
+	client, err := mongo.NewClient(clientOptions)
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = client.Connect(context.Background())
+	if err != nil {
+		log.Fatal(err)
+	}
+	return client
+}
