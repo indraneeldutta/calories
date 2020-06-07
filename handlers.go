859d52295834c36958867f8e0008d4384fc287a5
Indraneel Dutta <indraneel.dutta@hotmail.com>
2020-06-07 22:11:31 +0530

    moved the handler functions

---
+++ handlers.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
+)
+
+func handleMeals(w http.ResponseWriter, r *http.Request) {
+	calorie, _ := strconv.ParseFloat(mux.Vars(r)["calorie"], 64)
+	response := GetMeals(context.Background(), calorie)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(response)
+}
+
+func handleStoreMeals(w http.ResponseWriter, r *http.Request) {
+	var reqBody RequestStoreMeals
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		json.NewEncoder(w).Encode("Invalid request")
+		return
+	}
+	err = json.Unmarshal(body, &reqBody)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		json.NewEncoder(w).Encode("Invalid request")
+		return
+	}
+
+	response := StoreMeals(context.Background(), reqBody)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(response.Status)
+	json.NewEncoder(w).Encode(response)
+}
+
+func handleGetUserMeals(w http.ResponseWriter, r *http.Request) {
+	userID, _ := strconv.ParseInt(mux.Vars(r)["userid"], 2, 64)
+	response := GetUserMeals(context.Background(), userID)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(response.Status)
+	json.NewEncoder(w).Encode(response)
+}
--- main.go
+++ main.go
@@ -1,13 +1,9 @@
 package main
 
 import (
-	"context"
-	"encoding/json"
-	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
-	"strconv"
 
 	"github.com/gorilla/mux"
 )
@@ -18,41 +14,4 @@ func main() {
 	router.HandleFunc("/meals/store", handleStoreMeals).Methods("POST")
 	router.HandleFunc("/user/meals/{userid}", handleGetUserMeals).Methods("GET")
 	log.Fatal(http.ListenAndServe(os.Getenv("PORT"), router))
-}
-
-func handleMeals(w http.ResponseWriter, r *http.Request) {
-	calorie, _ := strconv.ParseFloat(mux.Vars(r)["calorie"], 64)
-	response := GetMeals(context.Background(), calorie)
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	json.NewEncoder(w).Encode(response)
-}
-
-func handleStoreMeals(w http.ResponseWriter, r *http.Request) {
-	var reqBody RequestStoreMeals
-	body, err := ioutil.ReadAll(r.Body)
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		json.NewEncoder(w).Encode("Invalid request")
-		return
-	}
-	err = json.Unmarshal(body, &reqBody)
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		json.NewEncoder(w).Encode("Invalid request")
-		return
-	}
-
-	response := StoreMeals(context.Background(), reqBody)
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(response.Status)
-	json.NewEncoder(w).Encode(response)
-}
-
-func handleGetUserMeals(w http.ResponseWriter, r *http.Request) {
-	userID, _ := strconv.ParseInt(mux.Vars(r)["userid"], 2, 64)
-	response := GetUserMeals(context.Background(), userID)
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(response.Status)
-	json.NewEncoder(w).Encode(response)
 }
